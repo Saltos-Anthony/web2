c0ecf2244555ad4001d0abc4eed784301ae3e8a2
Saltos-Anthony <145144078+Saltos-Anthony@users.noreply.github.com>
2023-11-07 21:50:45 -0500

    Add files via upload

---
+++ Saltos2-main/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Register from './Register';
+import './components/Estilos.css'; // Asegúrate de que los estilos CSS estén en el archivo adecuado
+
+
+function App() {
+  return (
+    <div className="App">
+      <Register />
+    </div>
+  );
+}
+
+export default App;
